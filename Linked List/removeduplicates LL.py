2cca2a7d0d6947d5bc0e680a122095c51f5f6c62
Jay Tiwari <97514947+GaddiPoPo@users.noreply.github.com>
2024-07-23 18:17:52 +0530

    Add files via upload

---
+++ Linked List/removeduplicates LL.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        while curr:
+            while curr.next and curr.next.val == curr.val:
+                curr.next = curr.next.next  # Corrected assignment
+            curr = curr.next
+        return head
